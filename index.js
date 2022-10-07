620873ad3486d4645657521ffefcbaddb8f267aa
luckflos <luckflos2@gmail.com>
2022-10-07 14:25:34 -0500

    Game Timer and Game Over Completed

--- index.js
+++ index.js
@@ -125,14 +125,32 @@ function rectangularCollision({
     )
 }
 
-let timer = 10
-function decreasetimer() {
-        setTimeout(decreasetimer, 1000)
-        if (timer > 0) {
-            timer--
-        document.querySelector('#timer').innerHTML = timer
+function determineWinner({ player, enemy, timerId }) {
+    clearTimeout(timerId)
+    document.querySelector('#displayText').style.display = 'flex'
+    if (player.health === enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Tie'
+    }  else if (player.health > enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
+    }  else if (player.health < enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
     }
 }
+
+let timer = 60
+let timerId
+function decreasetimer() {
+    if (timer > 0) {
+        timerId = setTimeout(decreasetimer, 1000)
+        timer--
+        document.querySelector('#timer').innerHTML = timer
+    }
+
+    if (timer === 0){
+        determineWinner({ player, enemy, timerId })
+    }
+}
+
 
 decreasetimer()
 
@@ -184,7 +202,12 @@ function animate() {
         player.health -= 20
         document.querySelector('#playerHealth').style.width = player.health + '%'
     }
+    // end game based on health
+    if (enemy.health <= 0 || player.health <= 0) {
+        determineWinner({ player, enemy, timerId })
+    }
 }
+
 
 animate()
 
